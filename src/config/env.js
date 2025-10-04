13b3f39bf4d04cc29564cc641ef79fd3df1216cb
ggomez <ga_gdl@outlook.com>
2025-10-03 23:29:18 -0600

    fix: file jackage.json

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,12 @@
 
-  const port = process.env.PORT || 3000;
-  const verifyToken = process.env.VERIFY_TOKEN;
-  const apiToken = process.env.API_TOKEN;
-  const apiVersion = process.env.API_VERSION;
-  const phoneNumberId = process.env.PHONE_NUMBER_ID;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT || 3000,
+  verifyToken: process.env.VERIFY_TOKEN,
+  apiToken: process.env.API_TOKEN,
+  apiVersion: process.env.API_VERSION,
+  phoneNumberId: process.env.PHONE_NUMBER_ID,
+};
